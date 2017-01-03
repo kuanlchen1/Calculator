37518ae8f205a2fe3512244015eefc87634fe865
Kuan L. Chen <kuanlchen1@gmail.com>
2017-01-03 18:48:43 +0800

    Add files via upload

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Kuan L. Chen on 03/01/2017.
+//  Copyright © 2017 klchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    private var displayValue: Double {
+        set {
+            numPadLabel.text = String(newValue)
+        }
+        get {
+            return Double(numPadLabel.text!)!
+        }
+    }
+    
+    @IBOutlet private weak var numPadLabel: UILabel!
+    
+    @IBAction private func touchDigiAction(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            
+            let numInTheDisplay = numPadLabel.text!
+            numPadLabel.text = numInTheDisplay + digit
+        }
+        else {
+            
+            numPadLabel.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction private func operationAction(_ sender: UIButton) {
+        
+        if userIsInTheMiddleOfTyping {
+            
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        if let operationSymbolString = sender.currentTitle {
+            
+            brain.performOperation(symbol: operationSymbolString)
+        }
+        
+        displayValue = brain.result
+    }
+}
+
